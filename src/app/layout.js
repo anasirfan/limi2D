5602d7e09fe0c9fbe6b4bc3bde672c4929e23fe1
unknown <anasirfan101010@gmail.com>
2025-02-25 10:15:12 +0500

    fonts

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,12 @@
 import { Inter } from 'next/font/google';
+import { Poppins } from 'next/font/google';
 import "./globals.css";
 
-const inter = Inter({
+const poppins  = Poppins({
   subsets: ["latin"],
+  weight: ['400', '500', '600', '700'],
+  display: 'swap',
+  variable: '--font-poppins',
 });
 
 export const metadata = {
@@ -13,7 +17,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${poppins.variable}`}>{children}</body>
     </html>
   );
 }
